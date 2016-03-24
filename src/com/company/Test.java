d6e94a663f0ef3daf67935b9f3ac98b0fa2d04ef
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-03-24 09:05:15 +0600

    Added Test File to the project.

---
+++ src/com/company/Test.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+	// write your code here
+        double[] fibonacciNumbers;
+        Fibonacci fib = new Fibonacci();
+        fibonacciNumbers = fib.findFibonacciNumbers(100);
+        System.out.println("The fibonacci numbers upto 100 are : ");
+        for(double num : fibonacciNumbers) {
+            System.out.println(num);
+        }
+//        for(int i = 0; i < 50; i++) {
+//            System.out.println("The " + i + "th fibonacci number is : " + fib.fibonacciNumberRecur(i) );
+//        }
+    }
+}
