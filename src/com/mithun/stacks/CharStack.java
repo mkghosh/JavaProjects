a47a6b6262f5f969b0c857e7d326d6481617dc0b
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-03-28 23:41:02 +0600

    Updated CharStack class

--- src/com/mithun/stacks/CharStack.java
+++ src/com/mithun/stacks/CharStack.java
@@ -1,6 +1,7 @@
 package com.mithun.stacks;
 
 /**
+ * <h1>Stacks of characters.</h1>
  * Created by Mithun Kumer Ghose on 3/24/2016.
  */
 public class CharStack {
@@ -18,18 +19,45 @@ public class CharStack {
         topOfStack = -1;
     }
 
+    /**
+     * <h1>Pushes the element to the stack.</h1>
+     * @param element {@link Character} element to push to the stack.
+     */
     public void push(char element) {
         stackArray[++topOfStack] = element;
     }
+
+    /**
+     * <h1>Gives and removes the top element of the stack.</h1>
+     * @return {@link Character} the top element of the stack.
+     */
     public char pop() {
-        return stackArray[topOfStack--];
+        if(topOfStack != 0)
+            return stackArray[topOfStack--];
+        else
+            return 0;
     }
+
+    /**
+     * <h1>Gives the top element of the stack but not removes it.</h1>
+     * @return {@link Character} top element of the stack.
+     */
     public char peek() {
         return stackArray[topOfStack];
     }
+
+    /**
+     * <h1>Returns true if the stack has no elements false otherwise.</h1>
+     * @return {@link Boolean} true if stack has no elements false instead.
+     */
     public boolean isEmpty() {
         return topOfStack < 0;
     }
+
+    /**
+     * <h1>Returns true if the stack's capacity is full false otherwise.</h1>
+     * @return {@link Boolean} true if the stack is full.
+     */
     public boolean isFull() {
         return topOfStack == stackArray.length - 1;
     }
