6b8e35da5de0eeee13cee8fa2476ea82ce625823
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-03 20:06:56 +0600

    Queue interface added.

---
+++ src/com/mithun/queue/Queue.java
@@ -0,0 +1,9 @@
+package com.mithun.queue;
+
+/**
+ * <h1>All the functionality of the followed queue will be declared here.</h1>
+ * Created by Mithun Kumer Ghose on 4/3/2016.
+ */
+public interface Queue {
+
+}
