1a4eea26d3ad8687b3080852f6f7aa49ac84ab76
Mithun Kumer Ghose <mkumerg@gmail.com>
2016-04-04 23:35:49 +0600

    Queue interfaces functionality declared.

--- src/com/mithun/queue/Queue.java
+++ src/com/mithun/queue/Queue.java
@@ -4,6 +4,15 @@ package com.mithun.queue;
  * <h1>All the functionality of the followed queue will be declared here.</h1>
  * Created by Mithun Kumer Ghose on 4/3/2016.
  */
-public interface Queue {
+public interface Queue <T> {
+    void push(T t);
+
+    T pop();
+
+    T peek();
+
+    boolean isEmpty();
+
+    boolean isFull();
 
 }
